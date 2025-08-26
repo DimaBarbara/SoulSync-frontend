0345e9157ca2f4ad82720b8d4293ace4ba73eded
Dmytro <dmytrobarbara1@gmail.com>
2025-08-26 17:54:02 +0300

    fix

--- soul-sync/src/lib/services/UserService.ts
+++
@@ -1,9 +0,0 @@
-import { AxiosResponse } from "axios";
-import { IUser } from "../models/IUser";
-import $api from "../utils/api";
-
-export default class UserService {
-    static async fetchUsers(): Promise<AxiosResponse<IUser>> {
-        return $api.get<IUser>('/users')
-    }
-}
