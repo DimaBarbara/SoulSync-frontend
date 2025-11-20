4f3c05e77a7e72a2b29d4a7d8efe28ebe69a21f5
Dmytro <dmytrobarbara1@gmail.com>
2025-11-21 00:04:24 +0200

    fix

--- middleware.ts
+++ middleware.ts
@@ -1,21 +1,15 @@
-import { NextResponse } from 'next/server';
-import type { NextRequest } from 'next/server';
+import { NextRequest, NextResponse } from "next/server";
 
 export function middleware(request: NextRequest) {
-  console.log(1)
-  const token = request.cookies.get('refreshToken');
-  const pathname = request.nextUrl.pathname;
-  console.log(pathname, "pathname")
-  const isDashboardRoute = pathname.startsWith('/dashboard');
+  const authHeader = request.headers.get('authorization');
+  
+  const accessTokenPresent = authHeader && authHeader.startsWith('Bearer ');
+  
+  const isDashboardRoute = request.nextUrl.pathname.startsWith('/dashboard');
 
-  if (isDashboardRoute && !token) {
+  if (isDashboardRoute && !accessTokenPresent) {
     const loginUrl = new URL('/?showLogin=true', request.url);
     return NextResponse.redirect(loginUrl);
   }
   return NextResponse.next();
 }
-export const config = {
-  matcher: [
-    '/dashboard/:path*',
-  ],
-};
