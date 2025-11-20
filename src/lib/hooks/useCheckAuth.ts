cbec7e4e81fca37d9104672c5444a72bf8df11da
Dmytro <dmytrobarbara1@gmail.com>
2025-11-21 00:10:52 +0200

    fix

--- src/lib/hooks/useCheckAuth.ts
+++ src/lib/hooks/useCheckAuth.ts
@@ -1,16 +1,29 @@
 import { useEffect, useContext } from "react";
 import { Context } from "@/app/StoreProvider";
-import { IUser } from "../models/IUser";
+import { useRouter, usePathname } from "next/navigation";
 
 export const useCheckAuth = () => {
   const { store } = useContext(Context);
+  const router = useRouter();
+  const pathname = usePathname(); 
+  const privateRoutes = ["/dashboard"]; 
+  const isPrivateRoute = privateRoutes.some(route => pathname.startsWith(route));
 
   useEffect(() => {    
-    if (localStorage.getItem("token")) {
+    const tokenExists = localStorage.getItem("token");
+
+    if (tokenExists) {
       store.checkAuth();
     } else {
-      store.setAuth(false); 
-      store.setUser({} as IUser);
+      store.setAuth(false);
+      if (isPrivateRoute) {
+        router.push('/'); 
+      }
     }
-  }, [store]);
+  }, [store, isPrivateRoute, router, pathname]);
+  useEffect(() => {
+    if (isPrivateRoute && !store.isAuth && !store.isLoading) {
+       router.push('/');
+    }
+  }, [isPrivateRoute, store.isAuth, store.isLoading, router]);
 };
